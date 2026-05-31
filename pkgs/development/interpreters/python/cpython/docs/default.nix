8f19e86a92d7b6043d03c1560b37a597435a54bd
Nicolas Benes <nbenes.gh@xandea.de>
2026-06-01 00:05:46 +0200

    pythonDocs: init at 3.14, add panicgh as maintainer
    (cherry picked from commit 2f031afd374e56b1abee2dbc953283c213796c54)

--- pkgs/development/interpreters/python/cpython/docs/default.nix
+++ pkgs/development/interpreters/python/cpython/docs/default.nix
@@ -14,6 +14,9 @@ let
       python310 = import ./3.10-html.nix {
         inherit stdenv fetchurl lib;
       };
+      python314 = import ./3.14-html.nix {
+        inherit stdenv fetchurl lib;
+      };
     };
     pdf_a4 = {
       recurseForDerivations = true;
@@ -21,6 +24,9 @@ let
         inherit stdenv fetchurl lib;
       };
       python310 = import ./3.10-pdf-a4.nix {
+        inherit stdenv fetchurl lib;
+      };
+      python314 = import ./3.14-pdf-a4.nix {
         inherit stdenv fetchurl lib;
       };
     };
@@ -41,10 +47,16 @@ let
       python310 = import ./3.10-text.nix {
         inherit stdenv fetchurl lib;
       };
+      python314 = import ./3.14-text.nix {
+        inherit stdenv fetchurl lib;
+      };
     };
     texinfo = {
       recurseForDerivations = true;
       python310 = import ./3.10-texinfo.nix {
+        inherit stdenv fetchurl lib;
+      };
+      python314 = import ./3.14-texinfo.nix {
         inherit stdenv fetchurl lib;
       };
     };
